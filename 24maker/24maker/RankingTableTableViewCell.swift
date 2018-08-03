b8a56781a1b216df4e0816b3015abe659d2ebc4d
ll1124 <ll1124@hanmail.net>
2018-08-03 17:12:38 +0900

    commit

---
+++ 24maker/24maker/RankingTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RankingTableTableViewCell.swift
+//  24maker
+//
+//  Created by cscoi020 on 2018. 8. 3..
+//  Copyright © 2018년 Qyun. All rights reserved.
+//
+
+import UIKit
+
+class RankingTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
